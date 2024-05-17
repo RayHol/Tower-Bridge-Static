c55c3a27597722a4305e47fffa9831c4c08f0963
Ray Holder <46814365+RayHol@users.noreply.github.com>
2024-05-17 11:41:56 +0100

    Update ar.js

--- Scripts/ar.js
+++ Scripts/ar.js
@@ -25,7 +25,7 @@ function initializeAR(mediaArray) {
         const lookImage = document.getElementById('look_1');
         const userAgent = navigator.userAgent.toLowerCase();
         if (userAgent.includes('iphone')) {
-            lookImage.setAttribute('src', './assets/Asset 4.png');
+            lookImage.setAttribute('src', './assets/Asset 3.png');
             lookImage.setAttribute('material', 'transparent: true; alphaTest: 0.5;');
         } else {
             lookImage.setAttribute('src', './assets/Asset 1.svg');
